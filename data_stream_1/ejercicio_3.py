e26f5742b8878821c50821ed968019b48822b4c3
Marco <marco.a.formoso@gmail.com>
2017-01-16 12:57:21 +0100

    Cambios en ejercicio

--- data_stream_1/ejercicio_3.py
+++ data_stream_1/ejercicio_3.py
@@ -5,3 +5,14 @@ test = np.array([1,1,1,1,1,1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,5,5,5,6,6,6,6
 
 print np.var([3,4,3,3,3,3,4,])
 #print sum(np.histogram(test)[0])/4
+
+
+lista = 'abceebdabaeedbdbabdcecdeabdbacedddacbbeabbcdacbeab'
+counting = lista
+print counting
+print len(counting)
+print counting.count('a')
+print counting.count('b')
+print counting.count('c')
+print counting.count('d')
+print counting.count('e')
